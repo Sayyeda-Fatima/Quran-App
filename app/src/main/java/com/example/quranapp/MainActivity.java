2aa1718c88fc1a473bf62f20dfa34da4d610dadd
Fatima <sayyedafatima.official@gmail.com>
2022-12-06 23:47:07 +0500

    App startup completed

---
+++ app/src/main/java/com/example/quranapp/MainActivity.java
@@ -0,0 +1,105 @@
+package com.example.quranapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    String currentDateTimeString = java.text.DateFormat.getDateTimeInstance().format(new Date());
+    TextView date;
+    EditText ayyatnumber;
+    Button searchBtn;
+    QuranData q = new QuranData();
+    String [] surah;
+    Integer surahStartingAyyat;
+    Integer nextSurahStartingAyyat;
+    ArrayAdapter<String> adpt;
+    Spinner surahDropDown;
+    int idOfSelectedSurah;
+    int ayyatToBeDiaplayed;
+    TextView validAyyat;
+    String ayyatNo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        date = findViewById(R.id.txtDateTime);
+        date.setText(currentDateTimeString);
+
+        surah = q.urduSurahNames;
+
+        adpt = new ArrayAdapter<String>(this,  R.layout.spinner_item, surah);
+
+        surahDropDown = findViewById(R.id.spinnerSurahName);
+
+        surahDropDown.setAdapter(adpt);
+
+        ayyatnumber = findViewById(R.id.txtAyyatInput);
+        searchBtn = findViewById(R.id.btnSearch);
+        validAyyat = findViewById(R.id.txtValid);
+
+        /*surahDropDown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                idOfSelectedSurah = i;
+                System.out.println(idOfSelectedSurah);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });*/
+
+        searchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validAyyat.setText("");
+                ayyatNo = String.valueOf(ayyatnumber.getText());
+                if (ayyatNo != ""){
+                    surahDropDown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        @Override
+                        public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                            idOfSelectedSurah = i;
+                        }
+
+                        @Override
+                        public void onNothingSelected(AdapterView<?> adapterView) {
+
+                        }
+                    });
+
+
+                    surahStartingAyyat = q.SSP[idOfSelectedSurah];
+                    nextSurahStartingAyyat = q.SSP[idOfSelectedSurah + 1];
+
+                    ayyatToBeDiaplayed = surahStartingAyyat + Integer.parseInt(ayyatnumber.getText().toString());
+                    if(ayyatToBeDiaplayed< nextSurahStartingAyyat){
+                        Intent i = new Intent(MainActivity.this, SecondActivity.class);
+                        i.putExtra("ayyatNo", Integer.toString(ayyatToBeDiaplayed-1));
+                        startActivity(i);
+                    }
+                    else {
+                        validAyyat.setText("Please give a valid ayyat number!");
+                    }
+                }
+                else {
+                    validAyyat.setText("Please give a valid ayyat number!");
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/quranapp/SecondActivity.java
@@ -0,0 +1,29 @@
+package com.example.quranapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    QuranData q = new QuranData();
+    TextView ayyatDisplay;
+    String ayyatno, ayyat;
+    Integer ayyatNo;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        ayyatDisplay = findViewById(R.id.txtAyyatDisplay);
+
+        ayyatno = getIntent().getStringExtra("ayyatNo");
+        ayyatNo = Integer.parseInt(ayyatno);
+
+        ayyat = q.QuranArabicText[ayyatNo];
+
+        ayyatDisplay.setText(ayyat);
+
+    }
+}
